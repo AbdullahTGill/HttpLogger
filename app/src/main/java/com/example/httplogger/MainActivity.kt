d1348bccf30050f06804d9cd398a8d8bb261479e
Abdullah Gill <abdullah.tariq@appinsnap.com>
2024-07-24 13:11:16 +0500

    fix: fixed some funtionality

--- app/src/main/java/com/example/httplogger/MainActivity.kt
+++ app/src/main/java/com/example/httplogger/MainActivity.kt
@@ -9,11 +9,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
@@ -31,6 +34,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,7 +43,6 @@ import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     private val viewModel: NetworkViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -48,16 +51,29 @@ class MainActivity : ComponentActivity() {
                 var selectedResponseInfo by remember { mutableStateOf<ResponseInfo?>(null) }
 
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding)) {
-                        FetchButton { viewModel.fetchRandomJoke(applicationContext) }
-
-                        if (selectedResponseInfo == null) {
-                            ListView(viewModel) { responseInfo ->
-                                selectedResponseInfo = responseInfo
+                    Box(modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)
+                    ) {
+                        Column(modifier = Modifier.fillMaxSize()) {
+                            if (selectedResponseInfo == null) {
+                                ListView(viewModel) { responseInfo ->
+                                    selectedResponseInfo = responseInfo
+                                }
+                            } else {
+                                ItemDetails(selectedResponseInfo) {
+                                    selectedResponseInfo = null
+                                }
                             }
-                        } else {
-                            ItemDetails(selectedResponseInfo) {
-                                selectedResponseInfo = null
+                        }
+                        if (selectedResponseInfo == null) {
+                            Button(
+                                onClick = { viewModel.fetchRandomJoke(applicationContext) },
+                                modifier = Modifier
+                                    .align(Alignment.BottomCenter)
+                                    .padding(16.dp)
+                            ) {
+                                Text("Make API Call")
                             }
                         }
                     }
@@ -67,8 +83,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-                @Composable
+@Composable
 fun ListView(viewModel: NetworkViewModel, onItemClick: (ResponseInfo) -> Unit) {
     val responseInfos by viewModel.responseInfoFlow.collectAsState()
 
@@ -79,7 +94,8 @@ fun ListView(viewModel: NetworkViewModel, onItemClick: (ResponseInfo) -> Unit) {
     ) {
         items(responseInfos) { responseInfo ->
             Column(
-                modifier = Modifier.padding(5.dp)
+                modifier = Modifier
+                    .padding(5.dp)
                     .fillMaxWidth()
                     .clickable { onItemClick(responseInfo) }
             ) {
@@ -109,13 +125,6 @@ fun ListView(viewModel: NetworkViewModel, onItemClick: (ResponseInfo) -> Unit) {
     }
 }
 
-@Composable
-fun FetchButton(onClick: () -> Unit) {
-    Button(onClick = onClick) {
-        Text("Fetch Random Joke")
-    }
-}
-
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ItemDetails(responseInfo: ResponseInfo?, onBack: () -> Unit) {
@@ -127,47 +136,42 @@ fun ItemDetails(responseInfo: ResponseInfo?, onBack: () -> Unit) {
     val coroutineScope = rememberCoroutineScope()
     var selectedTabIndex by remember { mutableStateOf(0) }
 
-    // Zindigi tab component, since the requirement here was an indicator so I used an indicator tab
-    ZindigiTabWithIndicator(
-        tabTitles = titles,
-        pagerState = pagerState,
-        selectedTabIndex = selectedTabIndex,
-        onTabSelected = { index ->
-            selectedTabIndex = index
-            coroutineScope.launch {
-                pagerState.animateScrollToPage(index)
+    Column {
+        // Add a back button at the top
+        Button(
+            onClick = onBack,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text("Back")
+        }
+
+        ZindigiTabWithIndicator(
+            tabTitles = titles,
+            pagerState = pagerState,
+            selectedTabIndex = selectedTabIndex,
+            onTabSelected = { index ->
+                selectedTabIndex = index
+                coroutineScope.launch {
+                    pagerState.animateScrollToPage(index)
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.fillMaxWidth()
+        ) { page ->
+            when (page) {
+                0 -> RequestInfoList()
+                1 -> JokeDisplay()
             }
-        },
-
-        modifier = Modifier.fillMaxWidth()
-    )
-
-    HorizontalPager(
-        state = pagerState,
-        modifier = Modifier.fillMaxWidth()
-    ) { page ->
-        when (page) {
-            0 -> RequestInfoList()
-            1 -> JokeDisplay()
-
         }
-    }
 
-    LaunchedEffect(pagerState.currentPage) {
-        coroutineScope.launch {
-            selectedTabIndex = pagerState.currentPage
-        }
-    }
-}
-
-
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    HttpLoggerTheme {
-        Column {
-            FetchButton { }
+        LaunchedEffect(pagerState.currentPage) {
+            coroutineScope.launch {
+                selectedTabIndex = pagerState.currentPage
+            }
         }
     }
 }
